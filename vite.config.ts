1337db72394f5a422e3caa13e0acb2070b8c8eb5
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-03-22 22:21:38 +0200

    feat: add zebar auto restart

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,34 @@
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
+import { execSync } from "node:child_process";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [preact(), tailwindcss()],
+	plugins: [
+		preact(),
+		tailwindcss(),
+		{
+			name: "postbuild",
+			closeBundle() {
+				if (process.env.CI) {
+					console.log("Skipping zebar.exe task because this is a CI build");
+					return;
+				}
+
+				const exePath = process.env.ZEBAR_EXE_PATH ?? "zebar.exe";
+
+				try {
+					// Kill the existing zebar.exe process
+					execSync(`taskkill /IM ${exePath} /F`, { stdio: "inherit" });
+				} catch (err) {
+					console.log(err);
+				}
+
+				// Start the new zebar.exe process
+				execSync(`start ${exePath}`, { stdio: "inherit" });
+			},
+		},
+	],
+	base: "./",
 });
