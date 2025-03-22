27380704f8ce0914838faf046a68e0db45dda535
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-03-23 01:40:13 +0200

    feat: add more animations

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -1,20 +1,36 @@
 import { type Workspace as GlazeWmWorkspace } from "glazewm";
 import { motion } from "motion/react";
+import type { MutableRef } from "preact/hooks";
 import type { GlazeWmOutput } from "zebar";
 
 type Props = {
 	glazewm: GlazeWmOutput;
 	workspace: GlazeWmWorkspace;
+	i: number;
+	updateCounter: MutableRef<number>;
 };
 
-export default function Workspace({ glazewm, workspace }: Props) {
+export default function Workspace({ glazewm, workspace, i, updateCounter }: Props) {
 	return (
 		<motion.div
-			animate={{
-				scale: workspace.isDisplayed ? 1.2 : 1,
-				backgroundColor: workspace.isDisplayed ? "var(--color-sky)" : "var(--color-surface)",
+			layout
+			initial={{
+				transform: "translateY(-8rem)",
 			}}
-			className={`h-4 w-4 cursor-pointer rounded`}
+			animate={{
+				transform: "translateY(0rem)",
+				scale: workspace.isDisplayed ? 1.2 : 1,
+				backgroundColor: workspace.isDisplayed ? "var(--color-sky)" : "var(--color-surface0)",
+				transition: {
+					transform: {
+						duration: 0.2,
+
+						// stagger if and only if first animation
+						delay: updateCounter.current === 0 ? i * 0.05 : 0,
+					},
+				},
+			}}
+			className="grid h-4 w-4 cursor-pointer place-items-center overflow-hidden rounded"
 			onClick={() => {
 				// TODO when glazewm supports focus --container-id
 				void glazewm.runCommand(`focus --workspace ${workspace.name}`);
--- src/components/Workspaces.tsx
+++ src/components/Workspaces.tsx
@@ -1,14 +1,25 @@
+import { useEffect, useRef } from "preact/hooks";
 import { useZebar } from "../contexts/zebarContext";
 import Component from "./Component";
 import Workspace from "./Workspace";
 
 export default function Workspaces() {
 	const { glazewm } = useZebar();
+	const updateCounter = useRef(0);
+	useEffect(() => {
+		if (glazewm) updateCounter.current++;
+	}, [glazewm]);
 	return (
 		glazewm && (
-			<Component className="flex gap-2">
-				{glazewm.currentWorkspaces.map((workspace) => (
-					<Workspace key={workspace.id} glazewm={glazewm} workspace={workspace} />
+			<Component layout className="flex gap-2">
+				{glazewm.currentWorkspaces.map((workspace, i) => (
+					<Workspace
+						key={workspace.id}
+						glazewm={glazewm}
+						workspace={workspace}
+						i={i}
+						updateCounter={updateCounter}
+					/>
 				))}
 			</Component>
 		)
