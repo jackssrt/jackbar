99c2f9b0d5fba40012bdb5a7e041dc8ec1d504d6
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-03-23 17:54:59 +0200

    fix: make `Component` use a motion div

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -1,12 +1,13 @@
+import { motion } from "motion/react";
 import type { ComponentProps, PropsWithChildren } from "preact/compat";
 import { cn } from "../utils";
 
-type Props = PropsWithChildren<ComponentProps<"div">>;
+type Props = PropsWithChildren<ComponentProps<typeof motion.div>>;
 
 export default function Component(props: Props) {
 	return (
-		<div {...props} className={cn(props.className, "border-crust bg-base h-8 w-fit rounded p-2")}>
+		<motion.div {...props} className={cn(props.className, "border-crust bg-base h-8 w-fit rounded p-2")}>
 			{props.children}
-		</div>
+		</motion.div>
 	);
 }
