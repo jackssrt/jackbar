7de79aa97b0f0c8eb0c7522f6fbdeff1729a5e58
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-03-29 00:10:52 +0200

    feat: add date and month to date display

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -1,6 +1,8 @@
 import { MdCalendarToday } from "react-icons/md";
 import useDate from "../hooks/useDate";
 import IconTextComponent from "./IconTextComponent";
+import NumberTicker from "./NumberTicker";
+import Separator from "./Separator";
 import TickerTrack from "./TickerTrack";
 
 const DAY_MAP = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
@@ -11,6 +13,10 @@ export default function DateDisplay() {
 	return (
 		<IconTextComponent icon={<MdCalendarToday />}>
 			<TickerTrack values={DAY_MAP} i={number} />
+			<Separator />
+			<NumberTicker numDigits={2} value={date.getDate()} />
+			<span className="mx-1">/</span>
+			<NumberTicker numDigits={2} value={date.getMonth() + 1} />
 		</IconTextComponent>
 	);
 }
