cecc59b2ba8df5331f99264bdbaaa281c5613d52
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-03-28 17:34:17 +0200

    feat: add scroll handler to `VolumeDisplay`

--- src/components/VolumeDisplay.tsx
+++ src/components/VolumeDisplay.tsx
@@ -1,3 +1,4 @@
+import { clamp } from "motion";
 import { MdVolumeUp } from "react-icons/md";
 import { useZebar } from "../contexts/zebarContext";
 import PercentComponent from "./PercentComponent";
@@ -11,6 +12,12 @@ export default function VolumeDisplay() {
 				valuePercentUnits={audio.defaultPlaybackDevice.volume}
 				warningLevel={30}
 				criticalLevel={70}
+				onWheel={(event) => {
+					if (!audio.defaultPlaybackDevice) return;
+
+					const delta = event.deltaY > 0 ? -3 : 3;
+					void audio.setVolume(clamp(0, 100, audio.defaultPlaybackDevice.volume + delta));
+				}}
 			></PercentComponent>
 		)
 	);
