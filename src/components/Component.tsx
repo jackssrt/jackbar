bd7f17dc97af6c5bb41df81484a0c414042ca0d9
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-04-05 23:01:41 +0300

    fix: fix ProgressBar clipping

--- src/components/Component.tsx
+++ src/components/Component.tsx
@@ -6,8 +6,10 @@ type Props = PropsWithChildren<ComponentProps<typeof motion.div>>;
 
 export default function Component(props: Props) {
 	return (
-		<motion.div {...props} className={cn("border-crust bg-base text-text h-8 w-fit rounded p-2", props.className)}>
-			{props.children}
+		<motion.div {...props} className={cn("border-crust bg-base text-text h-8 w-fit rounded", props.className)}>
+			<div className={cn("h-full w-full transform-gpu overflow-hidden rounded p-2", props.className)}>
+				{props.children}
+			</div>
 		</motion.div>
 	);
 }
