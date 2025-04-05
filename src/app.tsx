77b227d7c057e987fbfd73c238b6c5edd47d99ac
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-04-06 01:23:29 +0300

    feat: add gap between component groups

--- src/app.tsx
+++ src/app.tsx
@@ -14,7 +14,7 @@ export function App() {
 			<motion.div
 				initial={{ transform: "translateY(-8rem)" }}
 				animate={{ transform: "translateY(0rem)" }}
-				className="grid h-[36px] [grid-template-columns:1fr_auto_1fr] overflow-hidden bg-transparent p-0.5 font-mono"
+				className="grid h-[36px] [grid-template-columns:1fr_auto_1fr] gap-0.5 overflow-hidden bg-transparent p-0.5 font-mono"
 			>
 				<div className="flex items-center gap-0.5">
 					<WorkspacesDisplay />
