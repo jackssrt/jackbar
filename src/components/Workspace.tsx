ec33fbeb79b887e93190b7f354716bebdb881314
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-04-12 19:08:19 +0300

    feat: add pseudo workspaces

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -2,31 +2,43 @@ import { type Workspace as GlazeWmWorkspace } from "glazewm";
 import { motion } from "motion/react";
 import type { GlazeWmOutput } from "zebar";
 
-type Props = {
-	glazewm: GlazeWmOutput;
-	workspace: GlazeWmWorkspace;
-};
+type Props =
+	| {
+			isPseudo: false;
+			glazewm: GlazeWmOutput;
+			workspace: GlazeWmWorkspace;
+	  }
+	| {
+			isPseudo: true;
+			glazewm: GlazeWmOutput;
+			workspace: number;
+	  };
 
-export default function Workspace({ glazewm, workspace }: Props) {
+export default function Workspace({ glazewm, workspace, isPseudo }: Props) {
+	const isDisplayed = isPseudo ? false : workspace.isDisplayed;
+	const name = isPseudo ? workspace.toString() : workspace.name;
 	return (
 		<motion.div
 			layout
 			initial={{
-				transform: `translateY(-8rem) scale(${workspace.isDisplayed ? "1.2" : "1"})`,
+				transform: `translateY(-8rem) scale(${isDisplayed ? "1.2" : "1"})`,
+
+				width: isPseudo ? "0.25rem" : "1rem",
 			}}
 			animate={{
-				transform: `translateY(0rem) scale(${workspace.isDisplayed ? "1.2" : "1"})`,
-				backgroundColor: workspace.isDisplayed ? "var(--color-sky)" : "var(--color-surface0)",
+				transform: `translateY(0rem) scale(${isDisplayed ? "1.2" : "1"})`,
+				backgroundColor: isDisplayed ? "var(--color-sky)" : "var(--color-surface0)",
 				transition: {
 					transform: {
 						duration: 0.2,
 					},
 				},
+				width: isPseudo ? "0.25rem" : "1rem",
 			}}
-			className="grid h-4 w-4 cursor-pointer place-items-center overflow-hidden rounded"
+			className="grid h-4 cursor-pointer place-items-center overflow-hidden rounded"
 			onClick={() => {
 				// TODO when glazewm supports focus --container-id
-				void glazewm.runCommand(`focus --workspace ${workspace.name}`);
+				void glazewm.runCommand(`focus --workspace ${name}`);
 			}}
 		></motion.div>
 	);
--- src/components/WorkspacesDisplay.tsx
+++ src/components/WorkspacesDisplay.tsx
@@ -1,3 +1,4 @@
+import type { Workspace as GlazewmWorkspace } from "glazewm";
 import { useZebar } from "../contexts/zebarContext";
 import Component from "./Component";
 import Workspace from "./Workspace";
@@ -13,9 +14,27 @@ export default function WorkspacesDisplay() {
 				animate="animate"
 				variants={{ animate: { transition: { staggerChildren: 0.5 } } }}
 			>
-				{glazewm.currentWorkspaces.map((workspace) => (
-					<Workspace key={workspace.id} glazewm={glazewm} workspace={workspace} />
-				))}
+				{glazewm.currentWorkspaces
+					.reduce<[(GlazewmWorkspace | number)[], number]>(
+						([acc, lastNumber], cur) => {
+							const number = parseInt(cur.name === "0" ? "10" : cur.name);
+
+							if (!isNaN(number))
+								for (let i = 0; i < number - lastNumber - 1; i++) acc.push(lastNumber + i + 1);
+
+							acc.push(cur);
+
+							return [acc, number] as const;
+						},
+						[[], 0],
+					)[0]
+					.map((workspace) =>
+						typeof workspace !== "number" ? (
+							<Workspace key={workspace.id} glazewm={glazewm} workspace={workspace} isPseudo={false} />
+						) : (
+							<Workspace key={workspace} glazewm={glazewm} workspace={workspace} isPseudo={true} />
+						),
+					)}
 			</Component>
 		)
 	);
