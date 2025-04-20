5491b4be8bf07b05f6dfda0240cd3335c93170b8
jackssrt <33695699+jackssrt@users.noreply.github.com>
2025-04-20 20:23:29 +0300

    chore: use custom clamp function

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,12 @@
 import { clsx, type ClassValue } from "clsx";
-import { clamp } from "motion";
 import { twMerge } from "tailwind-merge";
 import type { AudioOutput } from "zebar";
 
 export function cn(...args: ClassValue[]) {
 	return twMerge(clsx(args));
+}
+export function clamp(value: number, min: number, max: number) {
+	return Math.min(Math.max(value, min), max);
 }
 export function handleVolumeWheelEvent(audio: AudioOutput) {
 	return (event: WheelEvent) => {
@@ -12,6 +14,6 @@ export function handleVolumeWheelEvent(audio: AudioOutput) {
 		if (!audio.defaultPlaybackDevice) return;
 
 		const delta = event.deltaY > 0 ? -2 : 2;
-		void audio.setVolume(clamp(0, 100, audio.defaultPlaybackDevice.volume + delta));
+		void audio.setVolume(clamp(audio.defaultPlaybackDevice.volume + delta, 0, 100));
 	};
 }
